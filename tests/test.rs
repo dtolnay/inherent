c8d19f187d5a6f2a2e6fd1838e24962482d22e42
David Tolnay <dtolnay@gmail.com>
2019-07-14 11:02:32 -0700

    Add basic test

---
+++ tests/test.rs
@@ -0,0 +1,19 @@
+mod types {
+    use inherent::inherent;
+
+    trait Trait {
+        fn f<T: ?Sized>(self);
+    }
+
+    pub struct Struct;
+
+    #[inherent(pub)]
+    impl Trait for Struct {
+        fn f<T: ?Sized>(self) {}
+    }
+}
+
+#[test]
+fn test() {
+    types::Struct.f::<str>();
+}
