74e1cdb37798998878be49ab52bf32c0d97a8c58
David Tolnay <dtolnay@gmail.com>
2024-01-01 22:49:51 -0800

    Release 1.0.11

--- src/lib.rs
+++ src/lib.rs
@@ -72,7 +72,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/inherent/1.0.10")]
+#![doc(html_root_url = "https://docs.rs/inherent/1.0.11")]
 #![allow(
     clippy::default_trait_access,
     clippy::module_name_repetitions,
